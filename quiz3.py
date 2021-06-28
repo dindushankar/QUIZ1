1b49b3f72a177ec6527cc7952a949ee8d4294813
dindushankar <77906557+dindushankar@users.noreply.github.com>
2021-06-27 23:32:13 -0500

    Add files via upload

---
+++ quiz3.py
@@ -0,0 +1,7 @@
+
+a = int(input("Enter a number"))
+while a <= 100:
+
+    if a % 3 != 0 and a % 5 != 0:
+        print("numbers are: ", a)
+    a = a+1
---
+++ quiz5.py
@@ -0,0 +1,4 @@
+import math
+for i in range(0,51):
+    x = math.sqrt(i)
+    print(x)
